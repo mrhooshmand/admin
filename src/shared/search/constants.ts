20ed2d19aad1916b2103ac9df5e3b756dde20d0c
hushmand <hushmand@safarbooking.com>
2026-07-19 15:26:14 +0330

    add roles page

--- src/shared/search/constants.ts
+++ src/shared/search/constants.ts
@@ -2,7 +2,7 @@ import { SortDirection } from "./types";
 
 export const DEFAULT_PAGE = 1;
 
-export const DEFAULT_PAGE_SIZE = 10;
+export const DEFAULT_PAGE_SIZE = 15;
 
 export const DEFAULT_ORDER = "id";
 
